37ba145ba8d429c00b3a84fa07dd818f148acf0e
a <ruan65@gmail.com>
2019-08-04 16:52:31 +0300

    create screen for first anim

---
+++ lib/Anim1Screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AnimOneScreen extends StatefulWidget {
+  @override
+  _AnimOneScreenState createState() => _AnimOneScreenState();
+}
+
+class _AnimOneScreenState extends State<AnimOneScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.orange,);
+  }
+}
